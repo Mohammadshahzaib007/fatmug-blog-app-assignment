2f1e139743be4d59151b624b06ca12873ca75223
Mohammad Shahzaib <shahzaibmohammad22@gmail.com>
2021-03-10 18:24:24 +0530

    image preview functionaliti was added

--- src/components/Layout/TopBar.js
+++ src/components/Layout/TopBar.js
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { Container } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,6 +21,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function TopBar () {
   const classes = useStyles();
+
+  const { pathname } = useLocation();
 
   const navLinks = [
     { name: 'write', link: '/write', variant: 'contained', color: 'secondary' },
@@ -39,8 +41,23 @@ export default function TopBar () {
         <Container>
           <Toolbar spacing={0} disableGutters>
             <Typography variant="h6" className={classes.title}>
-              <b>FATMUG</b> | Greetings! UserName
+              <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+                <b>FATMUG</b> | Greetings! UserName
+              </Link>
             </Typography>
+            {pathname === '/write' && (
+              <Button
+                style={{
+                  marginLeft: '1.875rem',
+                  color: 'white',
+                  borderColor: 'white'
+                }}
+                variant="contained"
+                color="secondary"
+              >
+                Publish
+              </Button>
+            )}
             {navLinks.map((item, i) => (
               <Link to={item.link} key={i}>
                 <Button
--- src/pages/writeArticle.js
+++ src/pages/writeArticle.js
@@ -6,7 +6,8 @@ import {
   Button,
   FormControl
 } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
+import placeholder from '../assets/placeholder.png';
 
 const useStyles = makeStyles({
   label: {
@@ -22,6 +23,33 @@ const useStyles = makeStyles({
 
 function writeArticle () {
   const classes = useStyles();
+  const [{ alt, src, name }, setImg] = useState({
+    src: placeholder,
+    alt: 'Upload an Image',
+    name: ''
+  });
+
+  const [post, setPost] = useState({
+    title: '',
+    description: ''
+  });
+
+  const inputChangeHandler = (e) => {
+    setPost({
+      ...post,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleImg = (e) => {
+    if (e.target.files[0]) {
+      setImg({
+        src: URL.createObjectURL(e.target.files[0]),
+        alt: e.target.files[0].name,
+        name: e.target.files[0].name
+      });
+    }
+  };
 
   return (
     <section>
@@ -33,27 +61,34 @@ function writeArticle () {
                 Title
               </label>
               <TextField
+                name="title"
                 id="title"
                 style={{ marginBottom: '6.25rem' }}
                 inputProps={{ style: { fontSize: '1.5625rem' } }} // font size of input text
                 fullWidth
                 variant="filled"
+                onChange={inputChangeHandler}
+                value={post.title}
               />
 
-              <label htmlFor="discription" className={classes.label}>
-                Discription
+              <label htmlFor="description" className={classes.label}>
+                Description
               </label>
               <TextField
+                name="description"
                 rows="20"
-                id="discription"
+                id="description"
                 inputProps={{ style: { fontSize: '1.5625rem' } }} // font size of input text
                 fullWidth
                 variant="filled"
                 multiline
+                onChange={inputChangeHandler}
+                value={post.description}
               />
 
               <div style={{ marginTop: '100px' }}>
                 <input
+                  onChange={handleImg}
                   accept="image/*"
                   style={{ display: 'none' }}
                   id="contained-button-file"
@@ -64,7 +99,26 @@ function writeArticle () {
                   <Button variant="contained" color="primary" component="span">
                     Upload Image
                   </Button>
+                  {name}
                 </label>
+              </div>
+              <img />
+              <div
+                style={{
+                  width: '100%',
+                  height: '31.25rem',
+                  marginTop: '5rem 0'
+                }}
+              >
+                <img
+                  src={src}
+                  alt={alt}
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'contain'
+                  }}
+                />
               </div>
             </FormControl>
           </Grid>
