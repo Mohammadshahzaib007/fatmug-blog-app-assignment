e2dd07f7d0fdce944536520a209ce0a4f10f1411
Mohammad Shahzaib <shahzaibmohammad22@gmail.com>
2021-03-12 18:00:34 +0530

    action creator was added

---
+++ src/store/actions/userAuth.js
@@ -0,0 +1,7 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const login = (email, password) => {
+  return dispatch => {
+    // ...side effects
+  };
+};
--- src/store/reducers/userAuthReducer.js
+++ src/store/reducers/userAuthReducer.js
@@ -1,9 +1,27 @@
+import * as actionTypes from '../actions/actionTypes';
+
 const initialState = {
-  user: ''
+  user: '',
+  isLoggedIn: false,
+  name: '',
+  blogs: []
 };
 
 const userAuthReducer = (state = initialState, action) => {
-  return state;
+  switch (action) {
+    case actionTypes.LOGIN:
+      return {
+        ...state,
+        user: action.payload.user,
+        isLoggedIn: action.payload.isLoggedIn,
+        name: action.payload.name
+      };
+
+    case actionTypes.SIGNUP:
+      return state;
+    default:
+      return state;
+  }
 }
   ;
 
